d6bed8fe4798ec6e171ecace132fa153aee742de
Gaspar <gasparkf@gmail.com>
2024-11-27 16:14:34 -0400

    Solução do desafio

--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -8,6 +8,14 @@ public class Question3 {
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
-
+    public Mono<Void> userIsValid(final User user) {
+        return Mono.just(user)
+                .flatMap(user1 -> {
+                    if (user1.password() != null && user1.password().length() > 8) {
+                        return Mono.empty();
+                    } else {
+                        return Mono.error(new RuntimeException("Password length should be greater than 8"));
+                    }
+                });
+    }
 }
